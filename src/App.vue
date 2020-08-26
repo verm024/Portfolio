7adcc56ff2fb146ce898fd2f4d31f2033b462afb
verm024 <verm024>
2020-08-26 22:04:37 +0700

    gatau apa ini

--- src/App.vue
+++ src/App.vue
@@ -1,60 +1,68 @@
 <template>
-  <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
+    <v-app>
+        <v-app-bar
+          app
+          color="#21243D"
+          dark
+          flat
+        >
+            <div class="container d-flex align-center">
+                <div>
+                    <v-img alt="Muhammad Naufal" contain src="@/assets/logo-naufal.png" width="100px"/>
+                </div> 
+                <v-btn href="#" text class="ml-auto custom-nav-link">
+                    <span>Home</span>
+                </v-btn>
+                <v-btn href="#about" text class="custom-nav-link">
+                    <span>About</span>
+                </v-btn>
+                <v-btn href="https://github.com/vuetifyjs/vuetify/releases/latest" target="_blank" text class="custom-nav-link">
+                    <span>Works</span>
+                </v-btn>
+                <v-btn href="https://github.com/vuetifyjs/vuetify/releases/latest" target="_blank" text class="custom-nav-link">
+                    <span>Blog</span>
+                </v-btn>
+                <v-btn href="https://github.com/vuetifyjs/vuetify/releases/latest" target="_blank" text class="custom-nav-link">
+                    <span>Contact</span>
+                </v-btn>
+            </div>
+        </v-app-bar>
 
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
-    </v-app-bar>
-
-    <v-main>
-      <HelloWorld/>
-    </v-main>
-  </v-app>
+        <v-main>
+            <router-view>
+              
+            </router-view>
+        </v-main>
+    </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
 
 export default {
-  name: 'App',
-
-  components: {
-    HelloWorld,
-  },
-
-  data: () => ({
-    //
-  }),
+    data: () => ({
+      
+    }),
 };
 </script>
+
+<style>
+    @import url('https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,600;0,700;0,800;1,300;1,400;1,600;1,700;1,800&family=PT+Serif:ital,wght@0,400;0,700;1,400;1,700&display=swap');
+
+    .custom-nav-link{
+        font-family: 'Open Sans', sans-serif;
+        font-weight: 500 !important;
+        font-size: 14px !important;
+    }
+
+    .custom-nav-link::before{
+      background-color: #21243D !important;
+    }
+
+    .custom-nav-link span{
+      transition: .1s ease-in-out;
+    }
+
+    .custom-nav-link:hover span{
+        color: #FFD082 !important;
+    }
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,33 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div class="home">
+        <div class="header-container">
+            <Header/>
+        </div>
+        <div class="about-container" id="about">
+            <About/>
+        </div>
+    </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import Header from '@/components/Header.vue'
+import About from '@/components/About.vue'
 
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
+    components: {
+        Header,
+        About
+    }
 }
 </script>
+
+<style scoped>
+    .header-container{
+        background: radial-gradient(50% 50% at 50% 50%, rgba(33, 36, 61, 0.95) 0%, #21243D 100%);
+    }
+
+    .about-container{
+        background: #21243D;
+    }
+
+</style>
